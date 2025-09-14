944f202321cea93e9bfa0afb30c05992e1676428
rodrigo <24rodrigocurso@gmail.com>
2025-09-14 08:49:38 -0300

    Adicionei a função imagem

--- meu_projeto/urls.py
+++ meu_projeto/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('minha_app.urls')),  # 👈 Inclui as URLs da app
+    path('', include('minha_app.urls')),
 ]
+
+# Servir arquivos de mídia durante o desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- minha_app/models.py
+++ minha_app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     conteudo = models.TextField()
-    data_criacao= models.DateTimeField(auto_now_add=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to='posts/', blank=True, null=True)
 
     def __str__(self):
-        return  self.titulo # é só para mostrar o nome do post no admin e no console
+        return self.titulo
